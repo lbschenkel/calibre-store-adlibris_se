d64cc4d8ca3be52c647b919f89af1739283b24eb
Leonardo Brondani Schenkel <leonardo@schenkel.net>
2025-03-13 13:14:21 +0100

    Bump version

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ from calibre.customize import StoreBase
 
 class AdlibrisStore(StoreBase):
     name            = 'Adlibris.se'
-    version         = (2025, 3, 0)
+    version         = (2025, 3, 1)
     description     = 'Handla böcker, skönlitteratur och facklitteratur hos Adlibris bokhandel. Priserna ligger lägst på marknaden vilket har noterats i ett flertal test i media.'
     author          = 'Leonardo Brondani Schenkel <leonardo@schenkel.net>'
     actual_plugin   = 'calibre_plugins.lbschenkel_store_adlibris_se.adlibris:AdlibrisStorePlugin'
